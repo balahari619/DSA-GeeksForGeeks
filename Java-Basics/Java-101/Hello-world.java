af0378055ae19f3e47690673cce02335af1acb43
EC2 Default User <ec2-user@ip-172-31-3-93.ec2.internal>
2022-04-30 17:50:00 +0000

    Hello World Program

---
+++ Java-Basics/Java-101/Hello-world.java
@@ -0,0 +1,10 @@
+// Java Hello World Program
+public class Test {
+	/* This is the main function.
+This is where the execution of Java program begins.*/
+	public static void main(String args[]) 
+	{
+		System.out.println("HelloWorld from java");
+	}
+}
+
