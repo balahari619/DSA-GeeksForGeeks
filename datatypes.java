2f221e6467b16179a5f21854cb95e753e26ed289
Bala Hari <balahari619@gmail.com>
2022-05-02 23:10:52 +0530

    Added datatypes and I/O

---
+++ datatypes.java
@@ -0,0 +1,11 @@
+public class datatypes {
+    public static void main (String args[]){
+        bool isValid = true;
+        byte marks = 95;
+        char grade = 'A';
+        float pi = 3.14;
+        float div = 15.0f/3.5f;
+        long views = 188900000;
+        int value = 12331;
+    } 
+}
